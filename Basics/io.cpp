e249a0df561c751128610d8a12ba518d8cab4ad5
Lakshita2002 <51129978+Lakshita2002@users.noreply.github.com>
2020-07-15 11:38:18 +0530

    Added input output file

---
+++ Basics/io.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // declaring a variable
+    int num;
+    // taking input
+    cin >> num;
+    // displaying output
+    cout << num << endl;
+
+    return 0;
+}
