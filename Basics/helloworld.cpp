cd10ff11d23c2e79ad70ab9c1156056515786d9e
Lakshita2002 <51129978+Lakshita2002@users.noreply.github.com>
2020-07-15 12:13:15 +0530

    Added comment info

--- Basics/helloworld.cpp
+++ Basics/helloworld.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main(){
 
+    // single line comments
+    /*
+    multiple line comments
+    */
     // cout is the output function
     cout << "Hello World";
     return 0;
