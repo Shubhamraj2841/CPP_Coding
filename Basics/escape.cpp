dd96c47fd3d6c4139834dbfa0f889f93b061e336
Lakshita2002 <51129978+Lakshita2002@users.noreply.github.com>
2020-07-16 12:15:56 +0530

    Add files via upload

---
+++ Basics/escape.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // string formatting and structuring
+    // new line
+    cout << "There's a \nmystery \nto be solved"  << endl;
+    // tab
+    cout << "There's a \t mystery to be solved";
+    return 0;
+}
---
+++ Basics/var_n_dtypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    bool n = true;
+    char c = 'l';
+    int x = 13;
+    // a constant variable
+    int const g = 78;
+    float y = 45.67;
+    double m = 2345.678;
+
+    // reserved keywords cannot be used as variable names
+    return 0;
+}
